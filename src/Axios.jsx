482ff575c28f219725db2389efbb6bbdbb573061
Ubuntu <ubuntu@ip-172-31-4-61.eu-north-1.compute.internal>
2025-10-28 05:45:26 +0000

    Updated Axios base URL

--- src/Axios.jsx
+++ src/Axios.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+
+import React from 'react'
 import axios from "axios";
+
+const local = "http://localhost:3000/";
+const production = "http://13.53.217.189/api/";
+
 const api = axios.create({
-  baseURL: "http://13.53.217.189",
+  baseURL: window.location.hostname === "localhost" ? local : production,
   withCredentials: true,
 });
+
+
 export default api;
